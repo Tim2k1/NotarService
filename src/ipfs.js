fadfb87fc22d5dec19c4d4e4946aa4f76d3aba2a
Tim2k1 <tim.braunger@student.fhws.de>
2023-12-13 08:57:02 +0100

    Edited wrong commentar in ipfs.js

--- src/ipfs.js
+++ src/ipfs.js
@@ -1,6 +1,6 @@
 const IPFS = require('ipfs-api');
 
-//using local Installation of infura waiting default on port 5001
+//using local Installation of IPFS waiting default on port 5001
 const ipfs = new IPFS({host: 'localhost', port: 5001, protocol: 'http'});
 
 export default ipfs;
