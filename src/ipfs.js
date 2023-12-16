2f7f25ee353d2e627fff8b5385dcdb6cb212f012
Tim2k1 <tim.braunger@student.fhws.de>
2023-12-16 11:06:37 +0100

    Added Commentars and another generated way to connect to ipfs

--- src/ipfs.js
+++ src/ipfs.js
@@ -3,4 +3,18 @@ const IPFS = require('ipfs-api');
 //using local Installation of IPFS waiting default on port 5001
 const ipfs = new IPFS({host: 'localhost', port: 5001, protocol: 'http'});
 
+//ChatGPT würde es so machen:
+/*
+
+const IPFS = require('ipfs-http-client');
+
+// Create an IPFS instance pointing to your local node
+const ipfs = IPFS.create({
+  host: 'localhost',
+  port: 5001,
+  protocol: 'http',
+});
+
+*/
+
 export default ipfs;
--- src/web3.js
+++ src/web3.js
@@ -4,3 +4,8 @@ import Web3 from 'web3';
 const web3 = new Web3(window.web3.currentProvider);
 
 export default web3;
+
+/*
+    Ich weiß nicht genau ob es so funktioniert, hab den Code aus den Beispiel Repositories
+    Falls es nicht funktioniert muss ich nochmal darüber schauen und vllt mal im Internet scaheuen
+*/
