5545c9fce95ebafeb0720ff2d03cf9f99c70bb52
Tim2k1 <tim.braunger@student.fhws.de>
2023-12-16 10:49:47 +0100

    new Examples for ipfs in Examples.js

--- src/Examples.js
+++ src/Examples.js
@@ -30,3 +30,36 @@ async function addFileToIPFS(filePath) {
     console.error('Error adding file to IPFS:', error);
   }
 }
+
+// Function to read a file from IPFS
+async function readFileFromIPFS(fileHash, outputPath) {
+  try {
+    // Get the file content from IPFS
+    const chunks = [];
+    for await (const chunk of ipfs.cat(fileHash)) {
+      chunks.push(chunk);
+    }
+
+    // Concatenate the chunks to get the complete file content
+    const fileContent = Buffer.concat(chunks);
+
+    // Write the file content to the specified output path
+    fs.writeFileSync(outputPath, fileContent);
+
+    console.log(`File retrieved from IPFS and saved to ${outputPath}`);
+  } catch (error) {
+    console.error('Error reading file from IPFS:', error.message);
+  }
+}
+
+// Function to unpin a file from IPFS
+async function unpinFileFromIPFS(cid) {
+  try {
+    // Unpin the file from IPFS
+    await ipfs.pin.rm(cid);
+
+    console.log(`File with CID ${cid} unpinned from IPFS`);
+  } catch (error) {
+    console.error('Error unpinning file from IPFS:', error.message);
+  }
+}
